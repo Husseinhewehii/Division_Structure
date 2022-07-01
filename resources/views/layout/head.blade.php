4965406e8efd19bdd8a1a69bf7df9c7fc4bd5dfd
Hussein El-Hewehii <husseinhewehii@gmail.com>
2022-07-01 22:11:02 +0200

    design page built

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,14 @@
+    
+    {{-- meta tags --}}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=2,shrink-to-fit=no,user-scalable=0,viewport-fit=cover">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    {{-- title --}}
+    <title>
+        @yield('title', 'Coding Test')
+    </title>
+    
+    {{-- styles --}}
+    <link rel="stylesheet" href='{{assets("layout/css/libs/bootstrap.min.css")}}'>
+    <link rel="stylesheet" href='{{assets("layout/css/main.min.css")}}'>
