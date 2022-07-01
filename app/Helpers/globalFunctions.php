370df0ba023029f5bc2458f78aa103974cb9b438
Hussein El-Hewehii <husseinhewehii@gmail.com>
2022-07-01 22:01:47 +0200

    layout and base help configured

---
+++ app/Helpers/globalFunctions.php
@@ -0,0 +1,7 @@
+<?php
+
+if(!function_exists("assets")){
+    function assets($path){
+        return url("assets/$path");
+    }
+ }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
