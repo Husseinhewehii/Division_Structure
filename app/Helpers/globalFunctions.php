6b6ec4e7b7bba6a9aeee4a758d1aaef05383f8c0
Hussein El-Hewehii <husseinhewehii@gmail.com>
2022-07-05 10:42:57 +0200

    seeder refactored

--- app/Helpers/globalFunctions.php
+++ app/Helpers/globalFunctions.php
@@ -9,6 +9,6 @@ if(!function_exists("assets")){
  if(!function_exists("seed_organization_with_employees")){
     function seed_organization_with_employees($model){
         $organizations = $model::all();
-        $organizations->each(fn($organization) => $organization->employees()->attach($organization->notEmployees()->get()->random(rand(3,8))));
+        $organizations->each(fn($organization) => $organization->employees()->attach($organization->notEmployees()->random(rand(3,8))));
     }
  }
