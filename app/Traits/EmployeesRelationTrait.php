b57887f9af695c246c42570056789488cb0bf5f3
Hussein El-Hewehii <husseinhewehii@gmail.com>
2022-07-05 10:41:28 +0200

    not employee query refactored again

--- app/Traits/EmployeesRelationTrait.php
+++ app/Traits/EmployeesRelationTrait.php
@@ -8,8 +8,11 @@ trait EmployeesRelationTrait{
 
     public function notEmployees()
     {
+        $pivotTable = self::$employees_relation_table;
+        $pivotKey = self::$pivot_key;
+
         return Employee::whereNot('id', $this->leader)
-        ->whereRaw(sprintf("id NOT IN (SELECT employees.id FROM employees LEFT join %s on %s.`employee_id` = `employees`.`id` where %s.%s = %s)", self::$employees_relation_table, self::$employees_relation_table, self::$employees_relation_table, self::$pivot_key, $this->id))
+        ->whereRaw("id NOT IN (SELECT employees.id FROM employees LEFT join $pivotTable on $pivotTable.`employee_id` = `employees`.`id` where $pivotTable.$pivotKey =  $this->id)")
         ->get();
     }
 
